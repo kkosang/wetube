0d45a7511264039279a742356f8a797b25bf1804
kkosang <87695921+kkosang@users.noreply.github.com>
2022-06-12 22:02:17 +0900

    ~#4.3

--- src/server.js
+++ src/server.js
@@ -1,21 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const app = express();
 const PORT = 3188;
 const logger = morgan("dev");
 
-const home = (req, res) => {
-  return res.send("hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.get("/", globalRouter); // 사용자의 get요청시 globalRouter 라우팅
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
