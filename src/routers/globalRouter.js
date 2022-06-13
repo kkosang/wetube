c2dfa171c76f3294101e6e894f1ccff2f9b490ba
kkosang <87695921+kkosang@users.noreply.github.com>
2022-06-13 23:30:59 +0900

    ~#4.6 Planning Routes

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { join } from "../controllers/userController";
-import { trending } from "../controllers/videoController";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 const globalRouter = express.Router(); // 라우터 만들기
 
 globalRouter.get("/", trending); // 라우터를 handler에 연결
 globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
