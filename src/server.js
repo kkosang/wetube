e46359c64098e706a32c5fd3a8d5fc5406476356
kkosang <87695921+kkosang@users.noreply.github.com>
2022-06-15 00:27:07 +0900

    ~#5.2 Partials

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
+console.log(process.cwd());
+
 const app = express();
 const PORT = 3188;
 const logger = morgan("dev");
 
+app.set("view engine", "pug"); // view engine을 pug로 세팅
+app.set("views", process.cwd() + "/src/views"); // view 설정 바꾸기
 app.use(logger);
-
 app.get("/", globalRouter); // 사용자의 get요청시 globalRouter 라우팅
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
