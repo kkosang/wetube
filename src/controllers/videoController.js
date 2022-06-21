ac3f9f1bbc58f63610201a50e000e309e01196da
kkosang <87695921+kkosang@users.noreply.github.com>
2022-06-21 21:50:37 +0900

    ~#5.7 conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,14 @@
+const fakeUser = {
+  username: "sanghyun",
+  loggedIn: true,
+};
+
 export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Comes from your controller " });
+  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-  console.log(req.params);
   return res.send("Delete Video");
 };
