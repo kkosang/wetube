f54a0192a4fd29e6bf804280a378cac45cfcd55d
kkosang <87695921+kkosang@users.noreply.github.com>
2022-07-04 22:26:35 +0900

    #6.13 Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,20 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+/* callback
+console.log("start");
+Video.find({}, (error, videos) => {
+  return res.render("home",{pageTitle: "Home", videos});
+});
+console.log("finished");
+*/
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos: [] });
+  } catch {
+    return res.render("server-error");
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
