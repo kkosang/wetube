a8b717ed67da8d8f5f9c81021b35079ceeac3288
kkosang <87695921+kkosang@users.noreply.github.com>
2022-07-11 01:00:04 +0900

    #6.26 Search part one

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,42 +1,28 @@
 import Video from "../models/Video";
 
-/* callback
-console.log("start");
-Video.find({}, (error, videos) => {
-  return res.render("home",{pageTitle: "Home", videos});
-});
-console.log("finished");
-*/
-
 export const home = async (req, res) => {
-  try {
-    const videos = await Video.find({});
-    console.log(videos);
-    return res.render("home", { pageTitle: "Home", videos });
-  } catch {
-    return res.render("server-error");
-  }
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  if (video === null) {
+  if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
+
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  return res.render("edit", {
-    pageTitle: `Edit: ${video.title}`,
-    video,
-  });
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
+
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
@@ -64,18 +50,25 @@ export const postUpload = async (req, res) => {
       description,
       hashtags: Video.formatHashtags(hashtags),
     });
+    return res.redirect("/");
   } catch (error) {
     return res.render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
   }
-
-  return res.redirect("/");
 };
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = (req, res) => {
+  const { keyword } = req.query;
+  if (keyword) {
+    // search
+  }
+  return res.render("search", { pageTitle: "Search" });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { join, login } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
-const globalRouter = express.Router(); // 라우터 만들기
 
-globalRouter.get("/", home); // 라우터를 handler에 연결
+const globalRouter = express.Router();
+
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
