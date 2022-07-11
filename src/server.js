d3779bdc31e5bb2928cc600815c77a11049de4bc
kkosang <87695921+kkosang@users.noreply.github.com>
2022-07-12 01:08:40 +0900

    #7.0 Create Account

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -11,7 +11,7 @@ app.set("view engine", "pug"); // view engine을 pug로 세팅
 app.set("views", process.cwd() + "/src/views"); // view 설정 바꾸기
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); // middleware를 route를 사용하기 전에 사용
-app.use("/", globalRouter); // 사용자의 get요청시 globalRouter 라우팅
+app.use("/", rootRouter); // 사용자의 get요청시 globalRouter 라우팅
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
