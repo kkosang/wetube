73cb7211ab235ff13f0dc7b94eaefa8f6a2512bf
kkosang <87695921+kkosang@users.noreply.github.com>
2022-07-13 23:16:07 +0900

    #7.8 Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -11,6 +12,13 @@ app.set("view engine", "pug"); // view engine을 pug로 세팅
 app.set("views", process.cwd() + "/src/views"); // view 설정 바꾸기
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); // middleware를 route를 사용하기 전에 사용
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 app.use("/", rootRouter); // 사용자의 get요청시 globalRouter 라우팅
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
