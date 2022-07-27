0beb81bbe127e309c5f2a6d9b5ebde0cd0eb2852
kkosang <87695921+kkosang@users.noreply.github.com>
2022-07-27 14:10:50 +0900

    # 7.16 Github Login Part One

--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 3188;
+const PORT = 4000;
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
