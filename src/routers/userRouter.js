33ed42b84ceade8e0103fba547a749e90122707f
kkosang <87695921+kkosang@users.noreply.github.com>
2022-08-01 23:06:49 +0900

    # 8.0 Edit Profile GET

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { Router } from "express";
 import {
+  postEdit,
+  getEdit,
   edit,
   logout,
   see,
@@ -9,7 +11,7 @@ import {
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
