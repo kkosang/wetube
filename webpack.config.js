e689e53b0415b055ff5bba9364260d8b48a0c5c1
kkosang <87695921+kkosang@users.noreply.github.com>
2022-08-22 23:18:08 +0900

    # 9.2 Webpack Configuration part Two

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
+const path = require("path");
+
 module.exports = {
   entry: "./src/client/js/main.js", // 변경하고자 하는 파일의 경로
+  mode: "development",
   output: {
     filename: "main.js",
-    path: "./assets/js", // 변형된 결과물의 저장 경로
+    path: path.resolve(__dirname, "assets", "js"), // 변형된 결과물의 저장 경로
+    // dirname : directory name 파일까지의 전체 경로
+    // path.resolve : 뒤의 몇개가 오던지 처음부터 끝까자 경로를 만들어줌
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 };
