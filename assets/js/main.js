1212388c007979fc3998f6ed912c36f5cb872ee9
kkosang <87695921+kkosang@users.noreply.github.com>
2022-08-25 20:58:59 +0900

    # 10.0 Introduction

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/client/js/main.js":
@@ -16,6 +15,7 @@
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n\nconsole.log(\"hi\");\n\n//# sourceURL=webpack://wetube/./src/client/js/main.js?");
 
 /***/ }),
@@ -24,9 +24,9 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scs
 /*!*************************************!*\
   !*** ./src/client/scss/styles.scss ***!
   \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://wetube/./src/client/scss/styles.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: expected \\\";\\\".\\n  ╷\\n9 │   a {\\r\\n  │     ^\\n  ╵\\n  src\\\\client\\\\scss\\\\styles.scss 9:5  root stylesheet\\n    at tryRunOrWebpackError (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5055:12)\\n    at __webpack_require__ (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5012:18)\\n    at C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5083:20\\n    at symbolIterator (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n    at done (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\neo-async\\\\async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:18:14)\\n    at C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:4990:43\\n    at symbolIterator (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\neo-async\\\\async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: expected \\\";\\\".\\n  ╷\\n9 │   a {\\r\\n  │     ^\\n  ╵\\n  src\\\\client\\\\scss\\\\styles.scss 9:5  root stylesheet\\n    at Object.<anonymous> (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\src\\\\client\\\\scss\\\\styles.scss:1:7)\\n    at C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\javascript\\\\JavascriptModulesPlugin.js:441:11\\n    at Hook.eval [as call] (eval at create (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\tapable\\\\lib\\\\HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\tapable\\\\lib\\\\Hook.js:14:14)\\n    at C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5057:39\\n    at tryRunOrWebpackError (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5055:12)\\n    at __webpack_require__ (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5012:18)\\n    at C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\webpack\\\\lib\\\\Compilation.js:5083:20\\n    at symbolIterator (C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\neo-async\\\\async.js:3485:9)\\n\\nGenerated code for C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\css-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\node_modules\\\\sass-loader\\\\dist\\\\cjs.js!C:\\\\Users\\\\82109\\\\Documents\\\\GitHub\\\\wetube\\\\src\\\\client\\\\scss\\\\styles.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nSassError: expected \\\\\\\";\\\\\\\".\\\\n  ╷\\\\n9 │   a {\\\\r\\\\n  │     ^\\\\n  ╵\\\\n  src\\\\\\\\client\\\\\\\\scss\\\\\\\\styles.scss 9:5  root stylesheet\\\");\");\n\n//# sourceURL=webpack://wetube/./src/client/scss/styles.scss?");
 
 /***/ })
 
