def997c826b25d1f89e4846e62f3b47eb0c9c4d5
kkosang <87695921+kkosang@users.noreply.github.com>
2022-08-29 22:06:30 +0900

    # 11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("videoPlayer");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    // video.paused boolean
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePlay = () => (playBtn.innerText = "Pause");
+const handlePause = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
