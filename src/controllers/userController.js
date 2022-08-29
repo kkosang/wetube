7488b90c4c9319fd39333ec28cbc505ae4c71ffb
kkosang <87695921+kkosang@users.noreply.github.com>
2022-08-29 21:19:57 +0900

    # 10.3 Styles Conclusions

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -200,7 +200,13 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params; // req.session.id로 가져오지 않은 이유는 public으로 보여주기 위함
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found" });
   }
