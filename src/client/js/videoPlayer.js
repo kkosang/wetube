247fbbde6e8e5fb8cf6f9779cea24e17d732c1ac
kkosang <87695921+kkosang@users.noreply.github.com>
2022-08-31 23:43:44 +0900

    # 11.6 Timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -45,10 +46,19 @@ const formaTime = (seconds) =>
 
 const handleLoadedMetadata = () => {
   totalTime.innerText = formaTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
   currentTime.innerText = formaTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -56,3 +66,4 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata); // video화면을 제외한 부분
 video.addEventListener("timeupdate", handleTimeUpdate); // video 시간이 변경될 때마다 JS가 event실행
+timeline.addEventListener("input", handleTimelineChange);
