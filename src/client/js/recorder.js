2c0c56e268e9438176d48d3f378f32f233616a53
kkosang <87695921+kkosang@users.noreply.github.com>
2022-09-21 17:03:42 +0900

    # 13.3 Recording Video part Two

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,30 +2,33 @@ const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview"); // upload의 video객체 생성
 
 let stream; // 다른 func에서 사용하기 위함
+let recorder;
+
+const handleDownload = () => {};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+
+  recorder.stop();
+};
 
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
 
-  const recorder = new MediaRecorder(stream); // MediaRecorder를 통해 오디오나 비디오를 녹화
-  recorder.ondataavailable = (e) => {
-    console.log("recording done !!");
-    console.log(e);
-    console.log(e.data);
+  recorder = new MediaRecorder(stream); // MediaRecorder를 통해 오디오나 비디오를 녹화
+  recorder.ondataavailable = (event) => {
+    const videoFile = URL.createObjectURL(event.data); // 단순히 브라우저의 메모리를 가리키기만 하고 있는 URL (파일을 가리키고 있는 url)
+    // preview를 recorder 영상으로 바꿈
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   }; // 녹화 종료시 ondata를 통해 정보를 가져옴
-  console.log(recorder);
   recorder.start(); // 녹화시작
-  console.log(recorder);
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
-};
-
-const handleStop = () => {
-  startBtn.innerText = "Start Recording";
-  startBtn.removeEventListener("click", handleStop);
-  startBtn.addEventListener("click", handleStart);
 };
 
 const init = async () => {
