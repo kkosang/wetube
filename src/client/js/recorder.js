ebb00feba947fba08a5d38618abe1670550fd5a7
kkosang <87695921+kkosang@users.noreply.github.com>
2022-09-21 15:52:17 +0900

    # 13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,14 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview"); // upload의 video객체 생성
 
 const handleStart = async () => {
   //마이크와 카메라에 접근
   const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
+    audio: false,
+    video: { width: 500, height: 800 },
   });
+  video.srcObject = stream; // video의 srcObj
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
